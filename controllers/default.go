d24b3494e3b57a218b5d555d24127bab6a385f42
tangjicheng <tangjch15@gmail.com>
2023-08-09 00:01:05 +0900

    fix: not found user id

--- controllers/default.go
+++ controllers/default.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"myserver/models"
+	"net/http"
 
 	beego "github.com/beego/beego/v2/server/web"
 )
@@ -15,6 +16,10 @@ func (c *MainController) Get() {
 	c.Data["Email"] = "tangjch15@gmail.com"
 
 	user := models.GetUserById(1)
+	if user == nil {
+		c.Ctx.ResponseWriter.WriteHeader(http.StatusNotFound)
+		c.Ctx.WriteString("Not find user id")
+	}
 	c.Data["user"] = user
 
 	c.TplName = "index.tpl"
